396e14d96c88dcd3a230504116190b8e5d0e4f29
ZenX <touhidsourav5@gmail.com>
2025-06-12 04:44:28 +0600

    fix:icon added to component

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,6 +1,3 @@
 import { Icon } from '@iconify/react';
-import HomeIcon from './HomeIcon';
-import CartIcon from './CartIcon';
-import SearchIcon from './SearchIcon';
 
-export { Icon, HomeIcon, CartIcon, SearchIcon };
+export { Icon };
