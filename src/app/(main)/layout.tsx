2ace3d13b4f2619731bc171e61cbdfb7f95a413f
Shakib Anwar Nibir <nibirshakib@gmail.com>
2025-06-18 07:07:25 +0600

    feat: implement signup and driver application pages, add ad tracker, and enhance layout structure

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,77 @@
+import { Box } from '@mantine/core';
+import { Footer, Navbar } from '@/components';
+import FixedSpinnerLogo from '@/components/FixedSpinnerLogo';
+
+export default function MainLayout({
+  children,
+  signupModal,
+}: Readonly<{
+  children: React.ReactNode;
+  signupModal: React.ReactNode;
+}>) {
+  return (
+    <Box maw={2000} mx={'auto'} className="relative">
+      <Navbar />
+      <Box>
+        {signupModal}
+        {children}
+        {/* <ActionIcon
+                component={Link}
+                href="/scan&spin-attributation"
+                variant="transparent"
+                className="!fixed right-1 bottom-2 z-50 transition-all duration-300 hover:scale-125 md:right-4 md:bottom-4"
+                size="2xl"
+              >
+                <Stack align="center" gap={0}>
+                  <svg
+                    width="100"
+                    height="32"
+                    viewBox="0 0 100 20"
+                    className="fill-none"
+                  >
+                    <path
+                      id="sadPath"
+                      d="M 10 40 Q 50 0 90 40" // Downward curve for sad face
+                      stroke="none"
+                      fill="none"
+                    />
+                    <text
+                      className="text-primary-800 fill-current text-[8px]"
+                      textAnchor="middle"
+                    >
+                      <textPath href="#sadPath" startOffset="50%">
+                        Scan & Spin
+                      </textPath>
+                    </text>
+                  </svg>
+                  <Image radius="100%" src="spinnerLogo2.svg" w={40} h={40} />
+                  <svg
+                    width="100"
+                    height="32"
+                    viewBox="0 0 100 20"
+                    className="fill-none"
+                  >
+                    <path
+                      id="smilePath"
+                      // d="M 10 20 Q 50 0 90 20" // Upward curve for smiley face
+                      d="M 10 -10 Q 50 20 90 -10"
+                      stroke="none"
+                      fill="none"
+                    />
+                    <text
+                      className="text-primary-800 fill-current text-[8px]"
+                      textAnchor="middle"
+                    >
+                      <textPath href="#smilePath" startOffset="50%">
+                        How it works
+                      </textPath>
+                    </text>
+                  </svg>
+                </Stack>
+              </ActionIcon> */}
+        <FixedSpinnerLogo />
+      </Box>
+      <Footer />
+    </Box>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,7 @@ import { Inter, Sora } from 'next/font/google';
 import '@mantine/core/styles.css';
 import './globals.css';
 import { Providers } from '@/providers';
-import { Box, ColorSchemeScript, mantineHtmlProps } from '@mantine/core';
-import { Footer, Navbar } from '@/components';
-import FixedSpinnerLogo from '@/components/FixedSpinnerLogo';
+import { ColorSchemeScript, mantineHtmlProps } from '@mantine/core';
 
 const poppins = Sora({
   variable: '--font-poppins',
@@ -28,10 +26,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-  signupModal,
 }: Readonly<{
   children: React.ReactNode;
-  signupModal: React.ReactNode;
 }>) {
   return (
     <html lang="en" {...mantineHtmlProps} suppressHydrationWarning>
@@ -45,71 +41,7 @@ export default function RootLayout({
       <body
         className={`${poppins.variable} ${inter.variable} relative overflow-clip antialiased`}
       >
-        <Providers>
-          <Box maw={2000} mx={'auto'} className="relative">
-            <Navbar />
-            <Box>
-              {signupModal}
-              {children}
-              {/* <ActionIcon
-                component={Link}
-                href="/scan&spin-attributation"
-                variant="transparent"
-                className="!fixed right-1 bottom-2 z-50 transition-all duration-300 hover:scale-125 md:right-4 md:bottom-4"
-                size="2xl"
-              >
-                <Stack align="center" gap={0}>
-                  <svg
-                    width="100"
-                    height="32"
-                    viewBox="0 0 100 20"
-                    className="fill-none"
-                  >
-                    <path
-                      id="sadPath"
-                      d="M 10 40 Q 50 0 90 40" // Downward curve for sad face
-                      stroke="none"
-                      fill="none"
-                    />
-                    <text
-                      className="text-primary-800 fill-current text-[8px]"
-                      textAnchor="middle"
-                    >
-                      <textPath href="#sadPath" startOffset="50%">
-                        Scan & Spin
-                      </textPath>
-                    </text>
-                  </svg>
-                  <Image radius="100%" src="spinnerLogo2.svg" w={40} h={40} />
-                  <svg
-                    width="100"
-                    height="32"
-                    viewBox="0 0 100 20"
-                    className="fill-none"
-                  >
-                    <path
-                      id="smilePath"
-                      // d="M 10 20 Q 50 0 90 20" // Upward curve for smiley face
-                      d="M 10 -10 Q 50 20 90 -10"
-                      stroke="none"
-                      fill="none"
-                    />
-                    <text
-                      className="text-primary-800 fill-current text-[8px]"
-                      textAnchor="middle"
-                    >
-                      <textPath href="#smilePath" startOffset="50%">
-                        How it works
-                      </textPath>
-                    </text>
-                  </svg>
-                </Stack>
-              </ActionIcon> */}
-              <FixedSpinnerLogo />
-            </Box>
-            <Footer />
-          </Box>
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
