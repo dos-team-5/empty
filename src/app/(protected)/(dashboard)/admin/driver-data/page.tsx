c0a1e81ed9945c6742b4c2b95303bacbf9f9dd54
Shakib Anwar Nibir <nibirshakib@gmail.com>
2025-06-20 23:38:28 +0600

    refactor: update layout and styling in DriverDataTable for improved responsiveness

--- src/app/(protected)/(dashboard)/admin/driver-data/page.tsx
+++ src/app/(protected)/(dashboard)/admin/driver-data/page.tsx
@@ -10,7 +10,7 @@ const DriverData = async (props: { searchParams: SearchParams }) => {
   const limit = searchParams.limit;
   const result = await getDrivers(Number(page) || 1, Number(limit) || 10);
   return (
-    <Box>
+    <Box mt={{ base: 60, md: 0 }}>
       {/* <DriverInformationCard /> */}
       <DriverDataTable
         data={result.data?.records ?? []}
