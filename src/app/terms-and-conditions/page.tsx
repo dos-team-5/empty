faa0f4b0941c6a6e255d4c0cfae9a1294da68d9a
Shakib Anwar Nibir <nibirshakib@gmail.com>
2025-06-22 19:05:20 +0600

    feat: create Terms and Conditions page and link in banking information form

---
+++ src/app/terms-and-conditions/page.tsx
@@ -0,0 +1,7 @@
+import TermsAndConditions from '@/components/TermsAndConditions';
+
+const TermsAndConditionsPage = () => {
+  return <TermsAndConditions />;
+};
+
+export default TermsAndConditionsPage;
