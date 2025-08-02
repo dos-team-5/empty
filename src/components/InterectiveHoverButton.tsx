b438f34dcc39c2230ac9f1e80d64bf6e4910d4c5
Md. Noman Ebeny Shahid <greatsaykot@gmail.com>
2025-08-02 15:46:41 +0600

    fix: update styling and content in button and success message components

--- src/components/InterectiveHoverButton.tsx
+++ src/components/InterectiveHoverButton.tsx
@@ -1,5 +1,5 @@
 import { Icon } from '@/components/FileManager/lib/Icon';
-import { cn } from '@/lib/utils';
+import { cn } from '@/lib';
 import React from 'react';
 
 interface InteractiveHoverButtonProps
@@ -13,18 +13,18 @@ export const InteractiveHoverButton = React.forwardRef<
     <button
       ref={ref}
       className={cn(
-        'group relative w-auto cursor-pointer overflow-hidden rounded-lg border-2 border-[#D37EB4] p-2.5 px-6 text-center font-medium text-[#D37EB4] text-sm',
+        'group relative w-auto cursor-pointer overflow-hidden rounded-lg border-2 border-[#D37EB4] px-[25px] py-2.5 text-center text-sm font-bold text-[#D37EB4]',
         className
       )}
       {...props}
     >
       <div className="flex items-center gap-2">
         <div className="h-2 w-2 rounded-full bg-[#D37EB4] transition-all duration-450 group-hover:scale-[100.8]"></div>
-        <span className="inline-block font-medium transition-all duration-450 group-hover:translate-x-12 group-hover:opacity-0 text-sm">
+        <span className="inline-block text-sm font-bold transition-all duration-450 group-hover:translate-x-12 group-hover:opacity-0">
           {children}
         </span>
       </div>
-      <div className="absolute top-0 z-10 flex h-full w-full translate-x-12 items-center justify-center gap-2 font-medium text-white opacity-0 transition-all duration-450 group-hover:-translate-x-5 group-hover:opacity-100 text-sm">
+      <div className="absolute top-0 z-10 flex h-full w-full translate-x-12 items-center justify-center gap-2 text-sm font-bold text-white opacity-0 transition-all duration-450 group-hover:-translate-x-5 group-hover:opacity-100">
         <span>{children}</span>
         <Icon icon="material-symbols:arrow-right-rounded" className="text-xl" />
       </div>
--- src/components/signupFormDriver/SuccessMessage.tsx
+++ src/components/signupFormDriver/SuccessMessage.tsx
@@ -1,38 +1,64 @@
-import { Card, Title, Stack, Image, Text, Box, Button } from '@mantine/core';
 import { useLanguage } from '@/providers/languageToggleContext';
+import { Box, Button, Card, Image, Stack, Text, Title } from '@mantine/core';
 import Link from 'next/link';
 
 const SuccessMessage = () => {
   const { language } = useLanguage();
-  
+
   const content = {
     en: {
-      title: "You have successfully signed up",
-      message: "Check your email for further information",
-      loginNow: 'Login Now'
+      title: 'You have successfully signed up',
+      message: 'You will now start to receive campaign offers.',
+      message2:
+        'Want to complete your profile now? You will have to finish this step before accepting your first campaign.',
+      loginNow: ' Complete My Profile',
+      loginMessage: '*Takes 4–5 minutes',
     },
     fr: {
-      title: "Vous vous êtes inscrit avec succès",
-      message: "Vérifiez votre email pour plus d'informations"
-    }
+      title: 'Vous vous êtes inscrit avec succès',
+      message: 'Vous pouvez maintenant recevoir des offres de campagnes.',
+      message2:
+        'Voulez-vous compléter votre profil maintenant? Vous devrez finir cette étape avant de pouvoir accepter votre première campagne.',
+      loginNow: 'Complétez mon profil',
+      loginMessage:
+        'Ceci est une opération en ligne et prendra environ 4-5 minutes.',
+    },
   };
 
   return (
-    <Card radius='lg' w='80%' mx='auto' p={{base:'sm', md:"xl"}} className="h-full">
-      <Stack align="center" gap={30} >
+    <Card
+      radius="lg"
+      w="100%"
+      mx="auto"
+      p={{ base: 'sm', md: 'xl' }}
+      className="h-full"
+    >
+      <Stack align="center" gap={30}>
         <Title order={2} ta="center">
           {content[language].title}
         </Title>
         <Box>
-          <Image src='/auth/successLogo.png' w={100}/>
+          <Image src="/auth/successLogo.png" w={100} />
         </Box>
-        <Text ta='center' fz={13}>{content[language].message}</Text>
-        <Stack align="center" gap={15}>
-        </Stack>
-       
-          <Button component={Link} href="https://dashboard.emptyad.com/driver-login"  bg="#D481B5" type="submit" >
-           Login Now
+        <Text ta="center" fz={13}>
+          {content[language].message}
+        </Text>
+        <Text ta="center" fz={13}>
+          {content[language].message2}
+        </Text>
+        <Stack align="center" gap={4}>
+          <Button
+            component={Link}
+            href="https://dashboard.emptyad.com/driver-login"
+            bg="#D481B5"
+            type="submit"
+          >
+            {content[language].loginNow}
           </Button>
+          <Text ta="center" fz={13}>
+            {content[language].loginMessage}
+          </Text>
+        </Stack>
       </Stack>
     </Card>
   );
